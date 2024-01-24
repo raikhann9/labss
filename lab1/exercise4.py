423bcc4aa92fc5bd0f86301b0cbda53f6a389c40
raikhann9 <nnon77rr@gmail.com>
2024-01-25 00:39:09 +0600

    First commit

---
+++ lab1/exercise4.py
@@ -0,0 +1,27 @@
+#task1
+x = 5
+print(type(x)) #answer:<class 'int'>
+
+#task2
+x = "Hello World"
+print(type(x)) #answer:<class 'str'>
+
+#task3
+x = 20.5
+print(type(x)) #answer:<class 'float'>
+
+#task4
+x = ["apple", "banana", "cherry"]
+print(type(x))  #answer:<class 'list'>
+
+#task5
+x = ("apple", "banana", "cherry")
+print(type(x))  #answer:<class 'tuple'>
+
+#task6
+x = {"name" : "John", "age" : 36}
+print(type(x))  #answer:<class 'dict'>
+
+#task7
+x = True
+print(type(x))  #answer:<class 'bool'>
---
+++ lab1/exercise6.py
@@ -0,0 +1,38 @@
+#task1
+x = "Hello World"
+print(len(x))
+
+#task2
+txt = "Hello World"
+x = txt[0]
+print(x)
+
+#task3
+txt = "Hello World"
+x = txt[2:5]
+print(x)
+
+#task4
+txt = " Hello World "
+x = txt.strip()
+print(x)
+
+#task5
+txt = "Hello World"
+txt = txt.upper()
+print(txt)
+
+#task6
+txt = "Hello World"
+txt = txt.lower()
+print(txt)
+
+#task7
+txt = "Hello World"
+txt = txt.replace("H", "J")
+print(txt)
+
+#task8
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
