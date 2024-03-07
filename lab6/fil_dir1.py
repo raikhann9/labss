a698bacfdb676c8e9cd8aaa3daf0a4d732aed013
raikhann9 <nnon77rr@gmail.com>
2024-03-07 10:13:52 +0500

    Modified fil1

--- lab6/fil_dir1.py
+++ lab6/fil_dir1.py
@@ -3,10 +3,10 @@ import os
 path_ = r"D:\PP2"
 all_i=os.listdir(path_)
 for i in all_i:
-    if os.path.isdir(path_):
-        direcs=os.path.join(path_, i)
-    if os.path.isfile(path_):
-        filess=os.path.join(path_, i)
+    if os.path.isdir(os.path.join(path_, i)):
+        direcs=i
+    if os.path.isfile(os.path.join(path_, i)):
+        filess=i
 
 print("directories:", direcs)
 print("files:", filess)
