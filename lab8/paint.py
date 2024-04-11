1d792ddbfd2f54c92330f68180edfebba49e0c7a
raikhann9 <nnon77rr@gmail.com>
2024-04-11 07:10:57 +0500

    new changes were made

---
+++ lab8/paint.py
@@ -0,0 +1,75 @@
+import pygame
+
+
+def rect(screen):
+    x_local, y_local = x, y
+    width, height = x2-x, y2-y
+    if x2 < x:
+        x_local = x2
+        width = x - x2
+    if y2 < y:
+        y_local = y2
+        height = y - y2
+
+    pygame.draw.rect(screen, colors[current_color], (x_local, y_local, width, height), width=3)
+
+def circle(screen):
+    pygame.draw.circle(screen, colors[current_color], (x,y), radius=((x2-x)**2+(y2-y)**2)**0.5, width=3)
+
+def eraser(screen):
+    pygame.draw.circle(screen_work, (0, 0, 0), (x2, y2), radius=10)
+
+
+pygame.init()
+screen = pygame.display.set_mode((1200, 800))
+screen_work = pygame.Surface(screen.get_size())
+instruments = pygame.Surface((295, 50))
+pygame.draw.rect(instruments, (255, 255, 255), (5, 5, 40, 40), width=3)
+pygame.draw.circle(instruments, (255, 255, 255), (75, 25), radius=20, width=3)
+pygame.draw.rect(instruments, (255, 255, 255), (105, 5, 30, 40), width=0)
+pygame.draw.rect(instruments, (200, 0, 0), (105, 5, 30, 15))
+pygame.draw.rect(instruments, (255, 0, 0), (145, 5, 40, 40))
+pygame.draw.rect(instruments, (0, 255, 0), (195, 5, 40, 40))
+pygame.draw.rect(instruments, (0, 0, 255), (245, 5, 40, 40))
+
+colors = {"red": (255, 0, 0), "green": (0, 255, 0), "blue": (0, 0, 255)}
+current_color = 'red'
+modes = {'rect':rect, 'circle':circle, 'eraser':eraser}
+current_mode = 'rect'
+clock = pygame.time.Clock()
+x, y, x2, y2 = -1, -1, -1, -1
+
+program = True
+while program:
+    clock.tick(120)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            program = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            if 145 <= x <= 285 and 755 <= y <= 800:
+                if x<190:
+                    current_color = 'red'
+                elif x<240:
+                    current_color = 'green'
+                else:
+                    current_color = 'blue'
+            elif 0 <= x <= 145 and 755 <= y <= 800:
+                if x<50:
+                    current_mode = 'rect'
+                elif x<100:
+                    current_mode = 'circle'
+                else:
+                    current_mode = 'eraser'
+
+            x2, y2 = x, y
+        if pygame.mouse.get_pressed()[0]:
+            x2, y2 = pygame.mouse.get_pos()
+            screen.blit(screen_work, (0, 0))
+            modes[current_mode](screen)
+            screen.blit(instruments, (0, 750))
+            pygame.display.flip()
+        if event.type == pygame.MOUSEBUTTONUP:
+            modes[current_mode](screen_work)
+    screen.blit(instruments, (0, 750))
+    pygame.display.flip()
---
+++ lab8/racer.py
@@ -0,0 +1,166 @@
+import pygame, sys
+from pygame.locals import *
+import random, time
+
+# Initialzing
+pygame.init()
+
+# Setting up FPS
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+# Creating colors
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Other Variables
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+SCORE = 0
+SCORE2 = 0
+
+# Background music
+pygame.mixer.Sound('lab8_bgsound.wav').play()
+
+# Fonts
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
+
+background = pygame.image.load("AnimatedStreet.png")
+
+# Create a white screen
+DISPLAYSURF = pygame.display.set_mode((400, 600))
+DISPLAYSURF.fill(WHITE)
+
+pygame.display.set_caption("Raikhan's Game")
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Enemy.png")
+        self.image = pygame.transform.scale(self.image, (100, 60))
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self):
+        global SCORE
+        self.rect.move_ip(0, SPEED)
+        if self.rect.top > 600:
+            SCORE += 1
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Coin.png")
+        self.image = pygame.transform.scale(self.image, (60, 60))
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self):
+        global SCORE2
+        self.rect.move_ip(0, SPEED)
+        if self.rect.top > 600:
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Player.png")
+        self.image = pygame.transform.scale(self.image, (100, 60))
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+
+# Setting up Sprites
+P1 = Player()
+E1 = Enemy()
+C1 = Coin()
+
+# Creating Sprites Groups
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+coins = pygame.sprite.Group()
+coins.add(C1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+all_sprites.add(C1)
+
+
+# Adding a new User event
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 500)
+
+# Game Loop
+while True:
+
+    # Cycles through all events occurring
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED += 0.5
+            new_coin = Coin()
+            coins.add(new_coin)
+            all_sprites.add(new_coin)
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    DISPLAYSURF.blit(background, (0, 0))
+    scores = font_small.render(str(SCORE), True, BLACK)
+    coinscore = font_small.render("Coins: " + str(SCORE2), True, BLACK)
+    DISPLAYSURF.blit(scores, (10, 10))
+    DISPLAYSURF.blit(coinscore, (SCREEN_WIDTH - 200, 10))
+
+    # Moves and Re-draws all Sprites
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move()
+
+
+    # To be run if collision occurs between Player and Enemy
+    if pygame.sprite.spritecollideany(P1, enemies):
+        pygame.mixer.Sound('lab8_crash.wav').play()
+        time.sleep(0.5)
+
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, (30, 250))
+
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
+
+    # Check for collision with coins
+    coin_collisions = pygame.sprite.spritecollide(P1, coins, True)
+    for coin in coin_collisions:
+        SCORE2 += 1
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
